b4349378597d8d1e21f2ba4355d6e7998b8972c8
Jacob Slunga <jacobslunga21@yahoo.se>
2025-12-24 00:00:32 +0100

    added better ux for chat

--- src/components/AI/chat/components/ResizeHandle.tsx
+++ src/components/AI/chat/components/ResizeHandle.tsx
@@ -15,7 +15,7 @@ export const ResizeHandle: FC<ResizeHandleProps> = ({
       onMouseDown={onStartResize}
     >
       <div
-        className={`absolute left-2 top-1/2 -translate-y-1/2 ${isResizing ? "w-2 bg-black dark:bg-white" : "w-1 group-hover:w-2 bg-black/40 dark:bg-white/40"} h-10  dark:group-hover:bg-white group-hover:bg-black transition-all rounded flex items-center justify-center`}
+        className={`absolute left-0 top-1/2 -translate-y-1/2 ${isResizing ? "w-2 bg-black dark:bg-white" : "w-1 group-hover:w-2 bg-black/40 dark:bg-white/40"} h-10  dark:group-hover:bg-white group-hover:bg-black transition-all rounded-r flex items-center justify-center`}
       />
     </div>
   );
