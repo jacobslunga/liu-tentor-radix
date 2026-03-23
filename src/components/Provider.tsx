1ae0fe124d562cd2d419bd64dcb227e599e582ed
Jacob Slunga <jacobslunga21@yahoo.se>
2026-03-23 14:39:13 +0100

    added camera

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,5 +1,4 @@
 import ChatWindowProvider from "@/context/ChatWindowContext";
-import { FontProvider } from "@/context/FontContext";
 import { LanguageProvider } from "@/context/LanguageContext";
 import { TextSizeProvider } from "@/context/TextSizeContext";
 import { ThemeProvider } from "@/context/ThemeContext";
@@ -8,11 +7,9 @@ export default function Provider({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
       <TextSizeProvider>
-        <FontProvider>
-          <ChatWindowProvider>
-            <LanguageProvider>{children}</LanguageProvider>
-          </ChatWindowProvider>
-        </FontProvider>
+        <ChatWindowProvider>
+          <LanguageProvider>{children}</LanguageProvider>
+        </ChatWindowProvider>
       </TextSizeProvider>
     </ThemeProvider>
   );
